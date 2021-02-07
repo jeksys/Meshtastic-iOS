2d45403752d13840afacf8c41dcb0fc78d3d84f9
Eugene Yagrushkin <yagrushkin@gmail.com>
2021-02-07 15:42:04 -0800

    no message

---
+++ Meshtastic/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Meshtastic-iOS
+//
+//  Created by Evgeny Yagrushkin on 2021-02-07.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Meshtastic/Meshtastic_iOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  Meshtastic_iOSApp.swift
+//  Meshtastic-iOS
+//
+//  Created by Evgeny Yagrushkin on 2021-02-07.
+//
+
+import SwiftUI
+
+@main
+struct Meshtastic_iOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            AppView()
+        }
+    }
+}
